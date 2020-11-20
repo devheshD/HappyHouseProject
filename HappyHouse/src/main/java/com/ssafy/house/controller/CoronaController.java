502b52153e599628a8642cd1ba7093934de2e34d
heshAlgo <serverdeveloper@naver.com>
2020-11-20 16:44:42 +0900

    corona jsp, controller 추가

---
+++ HappyHouse/src/main/java/com/ssafy/house/controller/CoronaController.java
@@ -0,0 +1,12 @@
+package com.ssafy.house.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/corona")
+public class CoronaController {
+
+	
+	
+}
