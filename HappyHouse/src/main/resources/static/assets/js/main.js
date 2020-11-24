8537a77d52daf71519713d9e1d21dad330705ba6
박지훈 <pjh073154@gmail.com>
2020-11-24 14:30:37 +0900

    추가 기능 (news, post, youtube, sites)

--- HappyHouse/src/main/resources/static/assets/js/main.js
+++ HappyHouse/src/main/resources/static/assets/js/main.js
@@ -14,7 +14,8 @@ $('#SignUpBtn').on('click', function() {
 !(function($) {
   "use strict";
 
-  // Smooth scroll for the navigation menu and links with .scrollto classes
+  // Smooth scroll for the navigation menu and links with .scrollto
+	// classes
   var scrolltoOffset = $('#header').outerHeight() - 1;
   $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
